81ce0c583681d69d3163639c7a3909a558791976
Silas Burger <ybsilas@gmail.com>
2019-12-03 09:42:15 -0800

    added .gitignore and main.go boilerplate

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "example title", Desc: "example description", Content: "example content"},
+		Article{Title: "example title1", Desc: "example description1", Content: "example content1"},
+		Article{Title: "example title2", Desc: "example description2", Content: "example content2"},
+	}
+	fmt.Println("Endpoint Hit: All Aritcles Endpoint")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "homepage Endpoint Hit!")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	handleRequests()
 }
