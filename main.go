97bcb62b5cf33307666e773664b619a84e4a34e2
Silas Burger <ybsilas@gmail.com>
2019-12-05 07:49:19 -0800

    added 404 page

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>homepage Endpoint Hit!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "for help contact the support team: <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>Oops, looks like you're lost. If we are giving you bad directions please let us know: <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a></h1>")
 	}
 }
 
