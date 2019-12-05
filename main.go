5158b39414a9014a346b539ecadaca16e04dcc5f
Silas Burger <ybsilas@gmail.com>
2019-12-05 07:21:07 -0800

    added a contact page

--- main.go
+++ main.go
@@ -1,37 +1,22 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
-type Article struct {
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-type Articles []Article
-
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	articles := Articles{
-		Article{Title: "example title", Desc: "example lasjkdflakkkkf", Content: "example content"},
-		Article{Title: "example title1", Desc: "example description1", Content: "example content1"},
-		Article{Title: "example title2", Desc: "example description2", Content: "example content2"},
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "<h1>homepage Endpoint Hit!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprintf(w, "for help contact the support team: <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
 	}
-	fmt.Println("Endpoint Hit: All Aritcles Endpoint")
-	json.NewEncoder(w).Encode(articles)
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "homepage Endpoint Hit!")
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", allArticles)
+	http.HandleFunc("/", handleFunc)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
