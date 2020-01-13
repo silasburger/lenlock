3f8b59d143b4bd164615336fe1dd81e0de57e769
Silas Burger <ybsilas@gmail.com>
2020-01-13 10:11:56 -0800

    added create user route

--- controllers/users.go
+++ controllers/users.go
@@ -36,5 +36,10 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "temporary response")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	fmt.Fprintln(w, r.PostForm["email"])
+	fmt.Fprintln(w, r.PostForm["password"])
 }
