c0a3520814512a3da628dd006fe2d4bab4362b89
Silas Burger <ybsilas@gmail.com>
2020-01-13 19:48:49 -0800

    create static page controller also updated the view type to implement the http.handler interface

--- main.go
+++ main.go
@@ -1,53 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"use-go/lenslocked.com/controllers"
 
 	"github.com/gorilla/mux"
-	"use-go/lenslocked.com/views"
 )
-
-var (
-	homeView    *views.View
-	contactView *views.View
-	faqView     *views.View
-)
-
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(homeView.Render(w, nil))
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(contactView.Render(w, nil))
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(faqView.Render(w, nil))
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "<h1>Oops, looks like you're lost. If we are giving you bad directions please let us know: <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a></h1>")
-}
 
 func handleRequests() {
-	homeView = views.NewView("bootstrap", "views/home.gohtml")
-	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	faqView = views.NewView("bootstrap", "views/faq.gohtml")
+	staticC := controllers.NewStatic()
 	usersC := controllers.NewUsers()
 
 	router := mux.NewRouter()
 	router.NotFoundHandler = http.HandlerFunc(notFound)
-	router.HandleFunc("/", home).Methods("GET")
-	router.HandleFunc("/contact", contact).Methods("GET")
-	router.HandleFunc("/faq", faq).Methods("GET")
+	router.Handle("/", staticC.HomeView).Methods("GET")
+	router.Handle("/contact", staticC.ContactView).Methods("GET")
 	router.HandleFunc("/signup", usersC.New).Methods("GET")
 	router.HandleFunc("/signup", usersC.Create).Methods("POST")
 	http.ListenAndServe(":3000", router)
--- views/view.go
+++ views/view.go
@@ -11,6 +11,24 @@ var (
 	TemplateExt string = ".gohtml"
 )
 
+type View struct {
+	Template *template.Template
+	Layout   string
+}
+
+func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if err := v.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+// Render is used to render the view with the predefined layout.
+func (v *View) Render(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "text/html")
+	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+//NewView parses the template files
 func NewView(layout string, files ...string) *View {
 	files = append(files,
 		layoutPaths()...,
@@ -26,20 +44,10 @@ func NewView(layout string, files ...string) *View {
 	}
 }
 
-type View struct {
-	Template *template.Template
-	Layout   string
-}
-
 func layoutPaths() []string {
 	filePaths, err := filepath.Glob(LayoutDir + "*" + TemplateExt)
 	if err != nil {
 		panic(err)
 	}
 	return filePaths
-}
-
-// Render is used to render the view with the predefined layout.
-func (v *View) Render(w http.ResponseWriter, data interface{}) error {
-	return v.Template.ExecuteTemplate(w, v.Layout, data)
 }
